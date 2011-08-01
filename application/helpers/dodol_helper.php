4dc174524b16c0035fe96c6a5877cf6e329963aa
Zidni Mubarock <zidmubarock@gmail.com>
2011-08-02 03:55:31 +0700

    move misc un-OOP function from dodol library to here, and create nested menu_rend function

--- application/helpers/dodol_helper.php
+++ application/helpers/dodol_helper.php
@@ -81,4 +81,169 @@ if( ! function_exists('merge'))
 	
 }
 
+function custom_time($date, $nodate=false){
+	if(empty($date) || $date == null) {
+		if($nodate==false){
+	        return "No date provided";
+		}else{
+			return $nodate;
+		}
+	    }
+
+	    $periods         = array("second", "minute", "hour", "day", "week", "month", "year", "decade");
+	    $lengths         = array("60","60","24","7","4.35","12","10");
+
+	    $now             = time();
+	    $unix_date         = strtotime($date);
+
+	       // check validity of date
+	    if(empty($unix_date)) {    
+	        return "Bad date";
+	    }
+
+	    // is it future date or past date
+	    if($now > $unix_date) {    
+	        $difference     = $now - $unix_date;
+	        $tense         = "ago";
+
+	    } else {
+	        $difference     = $unix_date - $now;
+	        $tense         = "from now";
+	    }
+
+	    for($j = 0; $difference >= $lengths[$j] && $j < count($lengths)-1; $j++) {
+	        $difference /= $lengths[$j];
+	    }
+
+	    $difference = round($difference);
+
+	    if($difference != 1) {
+	        $periods[$j].= "s";
+	    }
+	    return "$difference $periods[$j] {$tense}";
+}
+function arrayObject($array){
+	return json_decode(json_encode($array));
+}
+function objectToArray($object){
+		$array=array();
+		foreach($object as $member=>$data)
+		{
+			$array[$member]=$data;
+		}
+		return $array;
+}
+function jsonToArray($json){
+	return json_decode($json, true);
+}
+function arrayToJson($array){
+	return json_encode($array);
+}
+function objectToJson($objects){
+	return json_encode($this->objectToArray($objects));
+}
+function print_arrayRecrusive($array){
+	$array = (is_object($array)) ? $this->objectToArray($array) : $array;
+	$output = '';
+	foreach($array as $key => $value){
+		$output .= '<div class="box2 mb10"><span class="bold">'.$key.'</span> =';
+		if(is_array($value) || is_object($value)){
+			$output .= $this->print_arrayRecrusive($value);
+		}else{
+			$output .= $value;
+		}
+		$output .= '</div>';
+	}
+	return $output;
+}
+function datetime($sort=false){
+	if($sort){
+		return date('Y-m-d H:i:s', strtotime($sort));
+	}else{
+		return date('Y-m-d H:i:s');
+	}
+}
+function show_date($date){
+	$date= new DateTime($date);
+	$date = date_create_from_format('Y-m-d H:i:s', $date);
+	$str_date = $date->format('l, F j,  Y');
+	return $str_date;
+}
+function nice_strlink($string){
+		$new_string = strtolower(str_replace(' ', '-', $string));
+		return $new_string;
+}
+function copy_this_link($string, $anchor = 'copy this' ){
+		$object = '<span class="toClipBoard button" alt="'.$string.'">'.$anchor.'</span>';
+		return $object;
+}
+function isAjax() {
+	return (isset($_SERVER['HTTP_X_REQUESTED_WITH']) &&
+		($_SERVER['HTTP_X_REQUESTED_WITH'] == 'XMLHttpRequest'));
+}
+function ajax_loader($width=50, $class="loader"){
+	$loader = '<img class="'.$class.'" src="'.base_url().'/assets/gen_img/loader.gif" alt="loader" width="'.$width.'">';
+	return $loader;
+}
+function menu_rend($source, $type = 'menu_hor', $style = array()){
+	$wrap = ($wrap = element('link_wrap', $style)) ? $wrap : 'span';
+	$wrap_open = '<'.$wrap.'>';
+	$wrap_close = '</'.$wrap.'>';
+	$out = '<ul class="'.$type.'">';
+	foreach($source as $s){
+		if($child = element('child', $s)) :
+			$out .= '<li>'.$wrap_open.'<a href="'.$s['link'].'">'.$s['anchor'].'</a>'.$wrap_close._menu_rend($child, 1, $style).'</li>';
+		else:
+			$out .= '<li>'.$wrap_open.'<a href="'.$s['link'].'">'.$s['anchor'].'</a>'.$wrap_close.'</li>';
+		endif;
+	}
+	$out .= '<div class="clear"></div></ul>';
+	return $out;
+}
+function _menu_rend($source , $level, $style){
+	$wrap = ($wrap = element('link_wrap', $style)) ? $wrap : 'span';
+	$wrap_open = '<'.$wrap.'>';
+	$wrap_close = '</'.$wrap.'>';
+	$level = $level+1;
+	$out = '<ul class="level_'.$level.'">';
+	foreach($source as $s){
+		if($child = element('child', $s)) :
+			$out .= '<li>'.$wrap_open.'<a href="'.$s['link'].'">'.$s['anchor'].'</a>'.$wrap_close._menu_rend($child, $level, $style).'</li>';
+		else:
+		$out .= '<li>'.$wrap_open.'<a href="'.$s['link'].'">'.$s['anchor'].'</a>'.$wrap_close.'</li>';
+		endif;
+	}
+	$out .= '</ul>';
+	return $out;
+}
+
+
+
+function load_text_editor($id){
+		$this->_ci->load->helper('url');
+		$this->_ci->load->helper('ckeditor');
+		//Ckeditor's configuration
+		$config = array(
+			'id' 		=> 	$id, 
+			'path'		=>	'assets/global_js/ckeditor', 
+			'config' 	=> array(
+				'toolbar' 	=> 	"Full", 'width' 	=> 	"100%", 'height' 	=> 	'200px'),
+			'toolbar'	=> 'Basic',
+			'styles' 	=> array(
+				// STYLE 1 
+				'style 1' => array (
+					'name' 		=> 	'Blue Title','element' 	=> 	'h2', 'styles' => array(
+						'color' 	=> 	'Blue','font-weight' 	=> 	'bold')
+						),
+				// STYLE 2
+				'style 2' => array (
+				'name' 	=> 	'Red Title','element' 	=> 	'h2','styles' => array(
+					'color' 	=> 	'Red',	'font-weight' 		=> 	'bold','text-decoration'	=> 	'underline')
+						)				
+					)
+			);
+
+			echo display_ckeditor($config);
+
+}
 
