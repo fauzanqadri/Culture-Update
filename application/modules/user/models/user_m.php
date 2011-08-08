24f15e7936ca722e439869f2173e5fb807d047ab
Zidni Mubarock <zidmubarock@gmail.com>
2011-08-09 04:22:05 +0700

    some better line

--- application/modules/user/models/user_m.php
+++ application/modules/user/models/user_m.php
@@ -7,6 +7,7 @@ class User_m extends CI_Model {
 	//php 5 constructor
 	function __construct() {
 		parent::__construct();
+		
 	}
 	
 	//php 4 constructor
@@ -29,7 +30,7 @@ class User_m extends CI_Model {
 			return false;
 		}
 	}
-	function update($data, $id_user){
+	function do_update($data, $id_user){
 		$this->db->where('email', $data['email']);
 		$this->db->where('id !=', $id_user);
 		$q = $this->db->get('user');
@@ -84,5 +85,51 @@ class User_m extends CI_Model {
 			return false;
 		}
 	}
+	
+	
+	// API //
+	function browse($param){
+		$q = $this->db->get('user');
+		if($q->num_rows() > 0):
 
+			$return = array(
+				'users' => $q->result(),
+				'num_rows' => $q->num_rows(),
+			);
+			return $return;
+		else:
+			return false;
+		endif;
+	}
+	function getbyid($id){
+		$this->db->where('id', $id);
+		$q = $this->db->get('user');
+		if($q->num_rows() == 1):
+			return $q->row();
+		else:
+			return false;
+		endif;
+	}
+	function update($id, $data){
+		if($this->getbyid($id)):
+			$data['m_date'] = date('Y-m-d H:i:s');
+			$this->db->where('id', $id);
+			if($this->db->update('user', $data)):
+				return $this->getbyid($id);
+			else:
+				return false;
+			endif;
+		else:
+			return false;
+		endif;
+	}
+	function delete($id){
+		if($user = $this->getbyid($id)):
+			$this->db->where('id', $id);
+			$this->db->delete('user');
+			return $user;
+		else:
+			return false;
+		endif;
+	}
 }
